83171136a8f424ccb560e11a73d5540ddc7d7d83
Orlando Soto <Orlando.Soto-Pastor@cfpb.gov>
2017-08-04 19:11:22 -0400

    adding bus info

--- product/api.py
+++ product/api.py
@@ -1,5 +1,7 @@
 # my micro-service
-
+import requests
+import json
+import http.client, urllib.parse, urllib.error, base64
 from flask import Flask
 from flask_restful import Resource, Api
 
@@ -7,12 +9,16 @@ app = Flask(__name__)
 api = Api(app)
 
 class Product(Resource):
+	
 	def get(self):
-		return{
-			'products': ['Monday',
-						'Tuesday',
-						'Wednesday']
-		}
+		# Request headers
+		headers = {'api_key': '88c04b279955416f8605d0b76ebc8974'}
+		
+		# Request parameters
+		payload = {'StopID': '2000684'}
+
+		r = requests.get('https://api.wmata.com/NextBusService.svc/json/jPredictions?', params=payload, headers=headers)
+		return(r.json())
 
 api.add_resource(Product, '/')
 if __name__=='__main__':
--- website/index.php
+++ website/index.php
@@ -3,19 +3,25 @@
 		<title>Orlando Soto-Pastor</title>
 	</head>
 	<body>
-		<h1>Docker Compose - PHP with Microservice</h1>
+		<h1>Docker Compose - PHP with Microservices</h1>
 		<ul>
 			<?php
 			$json = file_get_contents('http://product-service');
-			$obj = json_decode($json);
+			$obj = json_decode($json, true);
+			
+			$stop = $obj['StopName'];
+			$route = $obj['Predictions'][0][RouteID];
+			$direction = $obj['Predictions'][0][DirectionText];
+			$minutes = $obj['Predictions'][0][Minutes];
 
-			$products = $obj->products;
-			foreach ($products as $product) {
-				echo "<li>$product</li>";
-			}
+			echo "<li>Stop: $stop</li>";
+			echo "<li>Route: $route</li>";
+			echo "<li>Direction: $direction</li>";
+			echo "<li>Next bus: $minutes minutes</li>";
+
 			?>
 		</ul>
-		<img src="http://s2.quickmeme.com/img/6d/6d63d092ef1a698433fd38b898451ab0c989df43a7389e5ca1324c9d4af35e42.jpg"> 
+		<img src="http://s2.quickmeme.com/img/6d/6d63d092ef1a698433fd38b898451ab0c989df43a7389e5ca1324c9d4af35e42.jpg" width="400" height="400"> 
 	</body>
 
 
