20ed90f639f25b0f9e9f9da07664fe39433bfa97
EC2 Default User <ec2-user@ip-172-31-22-253.us-east-2.compute.internal>
2017-08-06 22:12:18 +0000

    adding logic for time

--- product/api.py
+++ product/api.py
@@ -1,7 +1,8 @@
 # my micro-service
+import datetime
 import requests
 import json
-import http.client, urllib.parse, urllib.error, base64
+import http.client, base64
 from flask import Flask
 from flask_restful import Resource, Api
 
@@ -11,14 +12,19 @@ api = Api(app)
 class Product(Resource):
 	
 	def get(self):
-		# Request headers
-		headers = {'api_key': '88c04b279955416f8605d0b76ebc8974'}
+		# If before noon DC time
+                if(int(datetime.datetime.now().strftime("%H")) <= 16):
+                    my_stop = '2000684' #Randolph and Bluhill
+                else:
+                    my_stop = '2001185' # Glenmont station
+                # Request headers
+                headers = {'api_key': '88c04b279955416f8605d0b76ebc8974'}
 		
 		# Request parameters
-		payload = {'StopID': '2000684'}
+                payload = {'StopID': my_stop}
 
-		r = requests.get('https://api.wmata.com/NextBusService.svc/json/jPredictions?', params=payload, headers=headers)
-		return(r.json())
+                r = requests.get('https://api.wmata.com/NextBusService.svc/json/jPredictions?', params=payload, headers=headers)
+                return(r.json())
 
 api.add_resource(Product, '/')
 if __name__=='__main__':
