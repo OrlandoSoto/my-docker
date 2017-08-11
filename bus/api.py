f9c37e82df355f61adac05ef903c0aedba234bdc
Orlando Soto <orlando.soto-pastor@cfpb.gov>
2017-08-11 17:48:20 -0400

    Update api.py

--- bus/api.py
+++ bus/api.py
@@ -2,7 +2,6 @@
 import datetime
 import requests
 import json
-import base64
 from flask import Flask
 from flask_restful import Resource, Api
 
@@ -12,19 +11,23 @@ api = Api(app)
 class Product(Resource):
 	
 	def get(self):
-		# If before noon DC time
+		# If before noon DC time select the bus route from home to train station
                 if(int(datetime.datetime.now().strftime("%H")) <= 16):
                     my_stop = '2000684' #Randolph and Bluhill
-                else:
+                # Else select bus route from train station to home
+		else:
                     my_stop = '2001185' # Glenmont station
-                # Request headers
+                
+		# Request headers
                 headers = {'api_key': '88c04b279955416f8605d0b76ebc8974'}
 		
 		# Request parameters
                 payload = {'StopID': my_stop}
 
+                # Request the bus arrivala data
                 r = requests.get('https://api.wmata.com/NextBusService.svc/json/jPredictions?', params=payload, headers=headers)
-                return(r.json())
+                # Return the enitre JSON and the PHP code will parse it
+		return(r.json())
 
 api.add_resource(Product, '/')
 if __name__=='__main__':
